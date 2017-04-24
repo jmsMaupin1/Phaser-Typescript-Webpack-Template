eaacbb86cc59a9ca4c96f839d62378b74f8069f5
James Maupin <jmsMaupin1@yahoo.com>
2017-04-24 12:04:18 -0400

    Introduce Phaser Game states to comply with phaser convention

--- src/game.ts
+++ src/game.ts
@@ -1,21 +1,18 @@
-import '../assets/phaser-logo.jpg'
-class SimpleGame {
+import Boot from './states/Boot.ts'
+import Preloader from './states/PreLoader.ts'
+import MainMenu from './states/MainMenu.ts'
+import InitialGameState from './states/InitialGameState.ts'
+class SimpleGame extends Phaser.Game{
 
     constructor() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
-          preload: this.preload, create: this.create
-        });
-    }
+        super(800, 600, Phaser.AUTO, 'App', null);
+        super(800, 600, Phaser.AUTO, 'App', null);
+        this.state.add('Boot', Boot, false);
+        this.state.add('Preloader', Preloader, false);
+        this.state.add('MainMenu', MainMenu, false);
+        this.state.add('InitialGameState', InitialGameState, false);
 
-    game: Phaser.Game;
-
-    preload() {
-        this.game.load.image('logo', 'assets/phaser-logo.jpg');
-    }
-
-    create() {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+        this.state.start('Boot');
     }
 
 }
